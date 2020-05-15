e918d42e79de307cffdcaea62a46b58fb482f976
施佑學 <yoshi@biboting.com>
2020-05-15 13:56:09 +0800

    first commit

---
+++ lib/filelogger.dart
@@ -0,0 +1,72 @@
+library filelogger;
+
+import 'dart:io';
+
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:ssh/ssh.dart';
+
+class FileLogger {
+
+  static final FileLogger instance = FileLogger._privateConstructor();
+
+  FileLogger._privateConstructor();
+
+  String _privateKey;
+  String _logFileName = 'file_logger.txt';
+  String _toPath = '';
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/$_logFileName');
+  }
+
+  set privateKey(String key) {
+    _privateKey = key;
+  }
+
+  void log(String tag, String message) async {
+    final file = await _localFile;
+
+    String time = DateTime.now().toUtc().toIso8601String();
+    String content = '$time  $tag  $message\n';
+    file.writeAsString(content, mode: FileMode.append);
+  }
+
+  Future<bool> sendLog(String destPath, String destFileName, {bool clearLogs = true}) async {
+    var path = await _localPath;
+    var logFile = File('$path/$_logFileName');
+
+//    String formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
+    var sendFile = await logFile.copy('$path/$destFileName');
+
+    var client = new SSHClient(
+      host: '60.251.229.49',
+      port: 22,
+      username: 'yoshi',
+      passwordOrKey: {
+        'privateKey': _privateKey,
+      },
+    );
+
+    await client.connect();
+    await client.connectSFTP();
+    String result = await client.sftpUpload(path: sendFile.path, toPath: destPath);
+    if (result == 'upload_success') {
+      sendFile.delete();
+      if (clearLogs) {
+        logFile.writeAsString('');
+      }
+      return true;
+    } else {
+      sendFile.delete();
+      return false;
+    }
+  }
+}
